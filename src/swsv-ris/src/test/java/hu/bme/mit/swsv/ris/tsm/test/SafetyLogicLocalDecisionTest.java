7604e6fdd43541d32e5875186a00f414676cc100
Mate Stogica <mstogica@rapidminer.com>
2016-10-22 20:42:34 +0200

    adds review fixes #20 #22

--- src/swsv-ris/src/test/java/hu/bme/mit/swsv/ris/tsm/test/SafetyLogicLocalDecisionTest.java
+++ src/swsv-ris/src/test/java/hu/bme/mit/swsv/ris/tsm/test/SafetyLogicLocalDecisionTest.java
@@ -8,17 +8,6 @@ import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-
-import java.util.Arrays;
-import java.util.Collection;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-import org.mockito.InOrder;
-
 import hu.bme.mit.swsv.ris.common.Direction;
 import hu.bme.mit.swsv.ris.common.NeighborTSMInfo;
 import hu.bme.mit.swsv.ris.common.NeighborTSMStatus;
@@ -29,6 +18,16 @@ import hu.bme.mit.swsv.ris.common.SideTriple;
 import hu.bme.mit.swsv.ris.common.logging.LoggerWrapper;
 import hu.bme.mit.swsv.ris.tsm.SignalMapper;
 import hu.bme.mit.swsv.ris.tsm.impl.SafetyLogicImpl;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.mockito.InOrder;
 
 /**
  * Test class for distributed decision procedure implemented in
@@ -68,6 +67,9 @@ public class SafetyLogicLocalDecisionTest {
 
 	}
 
+	/**
+	 * Test for events that do not trigger sent signals.
+	 */
 	@Test
 	public void testNoStatusChangeSwitching() {
 		safetyLogic.sectionOccupancyChanged(Side.FACING, SectionOccupancy.FREE);
@@ -77,6 +79,9 @@ public class SafetyLogicLocalDecisionTest {
 		verify(signalMapper, times(0)).sendControl(any());
 	}
 
+	/**
+	 * Test for events that do not disable any sections. (REQ-TSM-03-01-01-05)
+	 */
 	@Test
 	public void testNoDisabledSectionSwitching() {
 		safetyLogic.sectionOccupancyChanged(Side.FACING, SectionOccupancy.OCCUPIED);
@@ -87,12 +92,15 @@ public class SafetyLogicLocalDecisionTest {
 		safetyLogic.turnoutDirectionChanged(Direction.STRAIGHT);
 		safetyLogic.sectionOccupancyChanged(Side.STRAIGHT, SectionOccupancy.OCCUPIED);
 		final InOrder inOrderSignalMapper = inOrder(signalMapper);
-		inOrderSignalMapper.verify(signalMapper, times(7))
-				.sendControl(eq(SideTriple.of(SectionControl.ENABLED, SectionControl.ENABLED, SectionControl.ENABLED)));
+		inOrderSignalMapper.verify(signalMapper, times(7)).sendControl(
+				eq(SideTriple.of(SectionControl.ENABLED, SectionControl.ENABLED, SectionControl.ENABLED)));
 
 		verify(signalMapper, times(7)).sendControl(any());
 	}
 
+	/**
+	 * Test for switching the turnout that had linked occupied sections before.
+	 */
 	@Test
 	public void testTurnoutSwitching() {
 
@@ -103,8 +111,8 @@ public class SafetyLogicLocalDecisionTest {
 		safetyLogic.sectionOccupancyChanged(Side.FACING, SectionOccupancy.FREE);
 
 		final InOrder inOrderSignalMapper = inOrder(signalMapper);
-		inOrderSignalMapper.verify(signalMapper)
-				.sendControl(eq(SideTriple.of(SectionControl.ENABLED, SectionControl.ENABLED, SectionControl.ENABLED)));
+		inOrderSignalMapper.verify(signalMapper).sendControl(
+				eq(SideTriple.of(SectionControl.ENABLED, SectionControl.ENABLED, SectionControl.ENABLED)));
 		inOrderSignalMapper.verify(signalMapper).sendControl(
 				eq(SideTriple.of(SectionControl.DISABLED, SectionControl.DISABLED, SectionControl.ENABLED)));
 		inOrderSignalMapper.verify(signalMapper).sendControl(
@@ -113,8 +121,8 @@ public class SafetyLogicLocalDecisionTest {
 				eq(SideTriple.of(SectionControl.DISABLED, SectionControl.DISABLED, SectionControl.ENABLED)));
 
 		// Facing section is free.
-		inOrderSignalMapper.verify(signalMapper)
-				.sendControl(eq(SideTriple.of(SectionControl.ENABLED, SectionControl.ENABLED, SectionControl.ENABLED)));
+		inOrderSignalMapper.verify(signalMapper).sendControl(
+				eq(SideTriple.of(SectionControl.ENABLED, SectionControl.ENABLED, SectionControl.ENABLED)));
 
 		verify(signalMapper, times(5)).sendControl(any());
 	}
@@ -125,6 +133,9 @@ public class SafetyLogicLocalDecisionTest {
 				{ Side.DIVERGENT, Direction.DIVERGENT, Direction.STRAIGHT } });
 	}
 
+	/**
+	 * Parametric test for REQ-TSM-03-01-01-03 and REQ-TSM-03-01-01-04.
+	 */
 	@Test
 	public void testOccupiedSectionsLinkage() {
 		safetyLogic.sectionOccupancyChanged(Side.FACING, SectionOccupancy.OCCUPIED);
@@ -138,15 +149,18 @@ public class SafetyLogicLocalDecisionTest {
 		final SideTriple<SectionControl> initialSectionControls = SideTriple.of(SectionControl.ENABLED,
 				SectionControl.ENABLED, SectionControl.ENABLED);
 		inOrderSignalMapper.verify(signalMapper, atLeastOnce()).sendControl(eq(initialSectionControls));
-		inOrderSignalMapper.verify(signalMapper)
-				.sendControl(eq(SideTriple.of(SectionControl.DISABLED, SectionControl.ENABLED, SectionControl.ENABLED)
-						.with(occupiedSide, SectionControl.DISABLED)));
-		inOrderSignalMapper.verify(signalMapper)
-				.sendControl(eq(initialSectionControls.with(occupiedSide, SectionControl.DISABLED)));
+		inOrderSignalMapper.verify(signalMapper).sendControl(
+				eq(SideTriple.of(SectionControl.DISABLED, SectionControl.ENABLED, SectionControl.ENABLED).with(
+						occupiedSide, SectionControl.DISABLED)));
+		inOrderSignalMapper.verify(signalMapper).sendControl(
+				eq(initialSectionControls.with(occupiedSide, SectionControl.DISABLED)));
 
 		verify(signalMapper, atLeast(3)).sendControl(any());
 	}
 
+	/**
+	 * Test for REQ-TSM-03-01-01-01 and REQ-TSM-03-01-01-02.
+	 */
 	@Test
 	public void testSwitchTrailing() {
 		safetyLogic.sectionOccupancyChanged(Side.STRAIGHT, SectionOccupancy.OCCUPIED);
@@ -155,8 +169,8 @@ public class SafetyLogicLocalDecisionTest {
 		safetyLogic.turnoutDirectionChanged(Direction.STRAIGHT);
 
 		final InOrder inOrderSignalMapper = inOrder(signalMapper);
-		inOrderSignalMapper.verify(signalMapper)
-				.sendControl(eq(SideTriple.of(SectionControl.ENABLED, SectionControl.ENABLED, SectionControl.ENABLED)));
+		inOrderSignalMapper.verify(signalMapper).sendControl(
+				eq(SideTriple.of(SectionControl.ENABLED, SectionControl.ENABLED, SectionControl.ENABLED)));
 		inOrderSignalMapper.verify(signalMapper, times(2)).sendControl(
 				eq(SideTriple.of(SectionControl.ENABLED, SectionControl.DISABLED, SectionControl.ENABLED)));
 		inOrderSignalMapper.verify(signalMapper).sendControl(
