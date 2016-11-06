827e156efe2ca4d9566a3ff3ac5bdb8d9bddb6e7
Mate Stogica <mstogica@rapidminer.com>
2016-11-06 20:45:56 +0100

    adds some test scenarios

--- src/swsv-ris/src/it/java/hu/bme/mit/swsv/ris/integrationtest/IntegrationTest.java
+++ src/swsv-ris/src/it/java/hu/bme/mit/swsv/ris/integrationtest/IntegrationTest.java
@@ -5,10 +5,45 @@ import java.net.URLDecoder;
 import org.junit.Test;
 
 public class IntegrationTest {
+
 	@Test
-	public void TrackOneTest() throws Exception {
-		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track1.json").getFile(), "UTF-8");
-		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event1.json").getFile(), "UTF-8");
+	public void Test1_SingleTrain_TrackIsGranted() throws Exception {
+		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track.json").getFile(), "UTF-8");
+		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event_case1.json").getFile(),
+				"UTF-8");
+
+		final World w = new World(trackPath, eventPath, 1);
+		w.simulateWorld();
+
+	}
+
+	@Test
+	public void Test2_SingleTrain_TrackNotGranted() throws Exception {
+		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track.json").getFile(), "UTF-8");
+		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event_case2.json").getFile(),
+				"UTF-8");
+
+		final World w = new World(trackPath, eventPath, 1);
+		w.simulateWorld();
+
+	}
+
+	@Test
+	public void Test3_TwoTrains_NoMeeting() throws Exception {
+		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track.json").getFile(), "UTF-8");
+		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event_case3.json").getFile(),
+				"UTF-8");
+
+		final World w = new World(trackPath, eventPath, 1);
+		w.simulateWorld();
+
+	}
+
+	@Test
+	public void Test4_TwoTrains_Meeting() throws Exception {
+		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track.json").getFile(), "UTF-8");
+		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event_case4.json").getFile(),
+				"UTF-8");
 
 		final World w = new World(trackPath, eventPath, 1);
 		w.simulateWorld();
