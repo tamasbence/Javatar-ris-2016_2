ac3945d09209b9b3e3019867e941f9193377fd09
Mate Stogica <mstogica@rapidminer.com>
2016-11-08 10:56:06 +0100

    adds missing tests

--- src/swsv-ris/src/it/java/hu/bme/mit/swsv/ris/integrationtest/IntegrationTest.java
+++ src/swsv-ris/src/it/java/hu/bme/mit/swsv/ris/integrationtest/IntegrationTest.java
@@ -8,7 +8,8 @@ public class IntegrationTest {
 
 	@Test
 	public void Test1_SingleTrain_TrackIsGranted() throws Exception {
-		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track.json").getFile(), "UTF-8");
+		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track_case1.json").getFile(),
+				"UTF-8");
 		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event_case1.json").getFile(),
 				"UTF-8");
 		final String assertsPath = URLDecoder.decode(IntegrationTest.class.getResource("assert_case1.json").getFile(),
@@ -21,7 +22,8 @@ public class IntegrationTest {
 
 	@Test
 	public void Test2_SingleTrain_TrackNotGranted() throws Exception {
-		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track.json").getFile(), "UTF-8");
+		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track_case2.json").getFile(),
+				"UTF-8");
 		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event_case2.json").getFile(),
 				"UTF-8");
 		final String assertsPath = URLDecoder.decode(IntegrationTest.class.getResource("assert_case2.json").getFile(),
@@ -34,7 +36,8 @@ public class IntegrationTest {
 
 	@Test
 	public void Test3_TwoTrains_NoMeeting() throws Exception {
-		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track.json").getFile(), "UTF-8");
+		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track_case3.json").getFile(),
+				"UTF-8");
 		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event_case3.json").getFile(),
 				"UTF-8");
 		final String assertsPath = URLDecoder.decode(IntegrationTest.class.getResource("assert_case3.json").getFile(),
@@ -47,10 +50,25 @@ public class IntegrationTest {
 
 	@Test
 	public void Test4_TwoTrains_Meeting() throws Exception {
-		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track.json").getFile(), "UTF-8");
+		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track_case4.json").getFile(),
+				"UTF-8");
 		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event_case4.json").getFile(),
 				"UTF-8");
 		final String assertsPath = URLDecoder.decode(IntegrationTest.class.getResource("assert_case4.json").getFile(),
+				"UTF-8");
+
+		final World w = new World(trackPath, eventPath, assertsPath, 1);
+		w.simulateWorld();
+
+	}
+
+	@Test
+	public void Test5_ThreeTrains_Meeting() throws Exception {
+		final String trackPath = URLDecoder.decode(IntegrationTest.class.getResource("track_case5.json").getFile(),
+				"UTF-8");
+		final String eventPath = URLDecoder.decode(IntegrationTest.class.getResource("event_case5.json").getFile(),
+				"UTF-8");
+		final String assertsPath = URLDecoder.decode(IntegrationTest.class.getResource("assert_case5.json").getFile(),
 				"UTF-8");
 
 		final World w = new World(trackPath, eventPath, assertsPath, 1);
