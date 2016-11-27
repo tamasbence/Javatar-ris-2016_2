3780af9e69e89781c857be1fe2c81455db130fcc
Mate Stogica <mstogica@rapidminer.com>
2016-11-27 16:15:56 +0100

    adds missing file

--- src/swsv-ris/src/main/java/hu/bme/mit/swsv/ris/tsm/impl/MapperForSignalMapper.java
+++ src/swsv-ris/src/main/java/hu/bme/mit/swsv/ris/tsm/impl/MapperForSignalMapper.java
@@ -3,16 +3,34 @@ package hu.bme.mit.swsv.ris.tsm.impl;
 import hu.bme.mit.swsv.ris.common.NeighborTSMStatus;
 import hu.bme.mit.swsv.ris.common.SectionControl;
 import hu.bme.mit.swsv.ris.common.SideTriple;
+import hu.bme.mit.swsv.ris.signals.HeartBeatSignal;
+import hu.bme.mit.swsv.ris.signals.SectionOccupancySignal;
+import hu.bme.mit.swsv.ris.signals.TurnoutDirectionSignal;
+import hu.bme.mit.swsv.ris.tsm.SignalMapper;
 
-public class MapperForSignalMapper {
+public class MapperForSignalMapper implements SignalMapper {
 	public SideTriple<SectionControl> sectionControlDecision;
 	public SideTriple<NeighborTSMStatus> neighborsStatus;
 
+	@Override
 	public void sendControl(final SideTriple<SectionControl> decision) {
 		sectionControlDecision = decision;
 	}
 
+	@Override
 	public void sendStatus(final SideTriple<NeighborTSMStatus> statusForNeighbors) {
 		neighborsStatus = statusForNeighbors;
+	}
+
+	@Override
+	public void receiveSectionOccupancy(final SectionOccupancySignal signal) {
+	}
+
+	@Override
+	public void receiveTurnoutDirection(final TurnoutDirectionSignal signal) {
+	}
+
+	@Override
+	public void receiveHeartBeat(final HeartBeatSignal signal) {
 	}
 }
